6fbec02447518aa80e704bbfe18b6e61d64b8363
Prateek Sharma <139517202+pkbros@users.noreply.github.com>
2025-12-26 22:25:34 +0530

    initalize the timer component

--- js/main.js
+++ js/main.js
@@ -1,2 +1,22 @@
 // ZenDash Main Script
 
+// Generic function to load a coponent inot a container
+function loadComponent(path, containerId){
+    fetch(path) // uses fetch API to request componet file from path
+    .then(response => {
+        if(!response.ok){
+            throw new Error(`Failed to load ${path}: ${response}`);
+        }
+        return response.text();
+    })
+    .then(html => {
+        document.getElementById(containerId).innerHTML = html;
+    })
+    .catch(error => {
+        console.error("Error loading component: ", error);
+    });
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    loadComponent("../components/timer.html", "timer-widget");
+});
